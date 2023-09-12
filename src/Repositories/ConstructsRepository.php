122bbb54ceeeac53fa3ce87534571729f17d7e1c
George Palmer <george@typedcms.com>
2023-09-12 14:34:11 +0100

    Added helper method for to fetch and normalise hierarchies.

--- src/Repositories/ConstructsRepository.php
+++ src/Repositories/ConstructsRepository.php
@@ -4,11 +4,29 @@ declare(strict_types=1);
 
 namespace TypedCMS\LaravelStarterKit\Repositories;
 
+use Illuminate\Support\Collection;
+use TypedCMS\LaravelStarterKit\Models\Construct;
+
+use function collect;
+
 class ConstructsRepository extends Repository
 {
     protected string $collection;
 
     protected string $blueprint;
+
+    /**
+     * @param array<string, mixed> $parameters
+     */
+    public function hierarchy(array $parameters = []): Collection
+    {
+        $document = $this->all($parameters);
+
+        /** @var Collection<int, Construct> $constructs */
+        $constructs = $document->getData();
+
+        return $this->traverseHierarchy($document->getMeta()['hierarchy']->tree, $constructs);
+    }
 
     public function getBlueprint(): string
     {
@@ -32,5 +50,19 @@ class ConstructsRepository extends Repository
     final public function getSpecifiedEndpoint(): ?string
     {
         return null;
+    }
+
+    /**
+     * @param array<mixed> $tree
+     * @param Collection<int, Construct> $constructs
+     *
+     * @return Collection<mixed>
+     */
+    protected function traverseHierarchy(array $tree, Collection $constructs): Collection
+    {
+        return collect($tree)->map(fn (object $item) => (object) [
+            'construct' => $constructs->firstWhere('identifier', $item->construct),
+            'children' => $this->traverseHierarchy($item->children, $constructs),
+        ]);
     }
 }
