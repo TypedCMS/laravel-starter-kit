69e02790c4519adb9889e28aad0ca1d4d29bf61f
George Palmer <hello@piranhageorge.com>
2024-08-18 00:00:08 +0100

    Fixed refresh job on Laravel 10.

--- src/Jobs/RefreshCaches.php
+++ src/Jobs/RefreshCaches.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace TypedCMS\LaravelStarterKit\Jobs;
 
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
 use TypedCMS\LaravelStarterKit\Repositories\Contracts\Cacheable;
 
 class RefreshCaches implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable;
 
     public function __construct(private readonly Cacheable $repo)
     {
